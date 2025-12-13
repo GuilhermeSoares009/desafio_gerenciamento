ec05fb6aa5fe47926068ac06ce1638f55d4c03bd
Guilherme Soares <guilherme.sousa009@hotmail.com>
2025-12-13 19:06:59 -0300

    fix: ajuste para trabalhar com hash

--- backend/app/Models/Pessoa.php
+++ backend/app/Models/Pessoa.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Pessoa extends Model
 {
     protected $table = 'pessoas';
+
+    use HasFactory;
 
     protected $fillable = [
         'nome',
